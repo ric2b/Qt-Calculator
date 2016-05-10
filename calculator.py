8b0f204d99ca1388e39efbf869e4c56cd4b110a3
ric2b <ricardo.amendoeira@ist.utl.pt>
2016-05-10 23:52:21 +0100

    Data entry complete

--- calculator.py
+++ calculator.py
@@ -14,12 +14,27 @@ class Calculator(QtWidgets.QMainWindow):
         for button in self.ui.numbers.buttons():
             button.clicked.connect(self.add_digit)
 
+        self.ui.dot.clicked.connect(self.add_dot)
+
         self.number_string = '0'
         self.show()
 
     def add_digit(self):
+        if len(self.number_string) >= 8:
+            return
+
         number = self.sender().objectName().split('_')[1]
+
+        if self.number_string == '0':
+            self.number_string = number
+        else: 
+            self.number_string += number
         self.ui.lcd.display(self.number_string)
+
+    def add_dot(self):
+        if '.' not in self.number_string:
+            self.number_string += '.'
+        print(self.number_string)
 
 
 if __name__ == '__main__':
